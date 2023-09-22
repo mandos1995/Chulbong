0551073834735497e071d692e9dc0467d55967fb
mandos1995 <mandos19950620@gmail.com>
2023-09-22 15:19:53 +0900

    chore: 프로젝트 생성

---
+++ ChulBong/ChulBong/ChulBongApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChulBongApp.swift
+//  ChulBong
+//
+//  Created by 김민석 on 2023/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ChulBongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChulBong/ChulBong/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChulBong
+//
+//  Created by 김민석 on 2023/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
