7a165ec375671a134dd82bdf982d8d2ea04029b0
mandos1995 <mandos19950620@gmail.com>
2023-09-22 17:53:50 +0900

    feat: Apple LogIn 작업 진행중..

--- ChulBong/ChulBongSPM/Sources/CBLoginSPM/Views/LoginView.swift
+++ ChulBong/ChulBongSPM/Sources/CBLoginSPM/Views/LoginView.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 public struct LoginView: View {
     
     public init() { }
     
     public var body: some View {
-        Text("로그인 화면")
+        Text("로그인")
+            .font(.title)
+            .onTapGesture {
+                showAppleLogin()
+            }
+    }
+    
+    private func showAppleLogin() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.email, .fullName]
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.performRequests()
     }
 }
